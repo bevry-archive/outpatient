2ea7001543ccb3b25ec0310921d1b1a699bc0f79
Benjamin Lupton <b@lupton.cc>
2017-04-15 16:44:25 +0800

    minimal with multiple children arguments - eliminating need for manual children arrays

--- source/block.js
+++ source/block.js
@@ -8,7 +8,7 @@ module.exports = function (opts = {}) {
 	const {
 		permalink = document.url,
 		heading = document.title,
-		subheading = document.subheading,
+		subheading = document.subheading || document.description,
 		author = document.author,
 		cssClasses = (document.cssClasses || []),
 		editUrl = document.editUrl,
@@ -21,62 +21,39 @@ module.exports = function (opts = {}) {
 
 	// Render
 	const classes = ['block'].concat(cssClasses)
-	return h('article', {
-		class: classes.join(' ')
-	}, [
-		h('header', {class: 'block-header'}, [
-			parents.length ? h('nav', { class: 'parentcrumbs' }, [
-				h('ul', parents.map((parent) => h('li', [
-					h('a', {
-						class: 'permalink',
-						href: parent.url
-					}, [
-						h('h3', parent.title)
-					])
-				])
+	return h('article', { class: classes.join(' ') },
+		h('header.block-header',
+			(parents.length || null) && h('nav.parentcrumbs',
+				h('ul', parents.map((parent) =>
+					h('li',
+						h('a.permalink', { href: parent.url },
+							h('h3', parent.title)
+						)
+					)
 				))
-			]) : null,
-			permalink ? h('a', {
-				class: 'permalink hover-link',
-				href: permalink
-			}, [
-				h('h1', heading)
-			]) : h('h1', heading),
-			subheading ? h('h2', subheading) : '',
-			date ? h('span', { class: 'date' }, date) : '',
-			author ? h('a', {
-				class: 'author',
-				href: `/people/${author}`
-			}, author) : ''
-		]),
-		h('section', { class: 'block-content', innerHTML: content }),
-		h('footer', { class: 'block-footer' }, [
-			(prev || up || next) ? h('nav', { class: 'prev-next' }, [
-				prev ? h('a', {
-					class: 'prev',
-					href: prev.url
-				}, [
-					h('span', { class: 'icon' }),
-					h('span', { class: 'title' }, prev.title)
-				]) : '',
-				up ? h('a', {
-					class: 'up',
-					href: up.url
-				}, [
-					h('span', { class: 'icon' }),
-					h('span', { class: 'title' }, up.title)
-				]) : '',
-				next ? h('a', {
-					class: 'next',
-					href: next.url
-				}, [
-					h('span', { class: 'icon' }),
-					h('span', { class: 'title' }, next.title)
-				]) : ''
-			]) : ''
-		]),
-		editUrl ? h('aside', { class: 'block-edit' }, [
+			),
+			(permalink || h('h1', heading)) && h('a.permalink.hover-link', { href: permalink }, h('h1', heading)),
+			(subheading || null) && h('h2', subheading),
+			(date || null) && h('span.date', date),
+			(author || null) && h('a.author', { href: `/people/${author}` }, author)
+		),
+		h('section.block-content', { innerHTML: content }),
+		h('footer.block-footer', ((prev || up || next) || null) && h('nav.prev-next',
+			(prev || null) && h('a.prev', { href: prev.url },
+				h('span.icon'),
+				h('span.title', prev.title)
+			),
+			(up || null) && h('a.up', { href: up.url },
+				h('span.icon'),
+				h('span.title', up.title)
+			),
+			(next || null) && h('a.next', { href: next.url },
+				h('span.icon'),
+				h('span.title', next.title)
+			)
+		)),
+		(editUrl || null) && h('aside.block-edit',
 			h('a', { href: editUrl }, 'Edit and improve this page!')
-		]) : ''
-	])
+		)
+	)
 }
